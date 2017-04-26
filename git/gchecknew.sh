c645239ff5e64635abb51bb99794e3cd5c1dfd13
William Johnson dos Santos Okano <williamokano@softbox.com.br>
2017-04-26 10:26:07 -0300

    Added the gchecknew command

--- git/gchecknew.sh
+++ git/gchecknew.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PREPEND_NAME=$1
+POSTFIX_NAME=$1
 ACTUAL_BRANCH_NAME=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-NEW_BRANCH_NAME="$ACTUAL_BRANCH_NAME-$PREPEND_NAME"
+NEW_BRANCH_NAME="$ACTUAL_BRANCH_NAME-$POSTFIX_NAME"
 
-if [ -z $PREPEND_NAME ]; then
+if [ -z $POSTFIX_NAME ]; then
     echo "Missing branch prepend"
     exit 1
 fi
