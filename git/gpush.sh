4ce99de54b9f772f2b2848acdedb92249a8041d2
João Matheus Verdadeiro Visotaky <joaomatheus@softbox.com.br>
2017-09-28 16:54:10 -0300

    Adicionando parâmetros para não fazer o fetch e/ou não fazer o stash no comando < gfp >

--- git/gpush.sh
+++ git/gpush.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 BRANCH_NAME=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+FORCED=$1
+
+if [ -z $FORCED ]; then
+    git push -u origin $BRANCH_NAME
+else
+	if [ $FORCED == '-f' ]; then
+		git push -u -f origin $BRANCH_NAME
+	else
+		echo "Parameter < $FORCED > not recognized"
+	fi
+fi
+
 git push -u origin $BRANCH_NAME
